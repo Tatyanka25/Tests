1dac2a352f59075f7d453a25ccf8813f45b4ab4a
Tatyanka25 <lapshinatatyana25@mail.ru>
2023-04-11 19:54:15 +0300

    aaa

--- src/Junit.java
+++ src/Junit.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Junit {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Junit {
+
+    @Test
+    @DisplayName("Вторая строка ни разу не входит в первую")
+    public void the_Second_Line_Is_Never_Included_In_The_First() {
+        String s1 = "QWERTY";
+        String s2 = "AB";
+        assertEquals(0, Main.Character(s1, s2), "Failed");
+
+    }
+
+
+    @Test
+    @DisplayName("Вторая строка входит в первую только один раз")
+    public void the_Second_Line_Enters_The_First_One_Only_Once() {
+        String s1 = "QW";
+        String s2 = "QW";
+        assertEquals(1, Main.Character(s1, s2), "Failed");
+    }
+
+    @Test
+    @DisplayName("Вторая строка входит в первую несколько раз")
+    public void the_Second_Line_Enters_The_First_Several_Times() {
+        String s1 = "QWQWQWQWQWQWQWQW";
+        String s2 = "QW";
+        assertEquals(8, Main.Character(s1, s2), "Failed");
+
+    }
+
+    @Test
+    @DisplayName("Введена пустая вторая строка")
+    public void An_Empty_Second_Line_Has_Been_Entered (){
+        assertThrows(Exception.class, ()->{
+                    Main.Character ("ABCDEDTYS","");
+                }
+        );}
+    @Test
+    @DisplayName("Первая строка пустая")
+    public void the_First_Line_Is_Empty(){
+        String s1 = "";
+        String s2 = "QW";
+        assertEquals(0, Main.Character(s1, s2), "Faild");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str1 = "QWTYUIQWRFTGQWJKKMBQKMBQW";
+        String str2 = "QW";
+
+        int count = Character (str1, str2);
+        System.out.println(count);
+    }
+
+    public static int Character (String str1, String str2){
+        if (str2.isEmpty())
+            throw new RuntimeException();
+        return str1.split(str2, -1).length - 1;
     }
 }
